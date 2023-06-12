5abbcdb34583d03e55f9e2a9ec12d10929bda9e5
Haya sixt <104806778+Haya-Sixt@users.noreply.github.com>
2023-06-12 13:19:36 +0300

    ns

--- Scripts/⏱️.js
+++ Scripts/⏱️.js
@@ -29,6 +29,7 @@ wdgt.Init = ()=> {
 	$(wdgt.sid).text('');
 	//
 	$('<svg>')
+	.attr("xmlns","http://www.w3.org/2000/svg")
 	.attr("style","font-size: 1500%; width: 100%; height: 100%;")
 	.html(`
 	<defs>
