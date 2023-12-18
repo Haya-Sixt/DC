2dd3fac64c78ed9249cd7c0d8a8b81b0327b2c82
Haya-Sixt <azbmdk@gmail.com>
2023-12-18 13:17:01 +0200

    🐈‍⬛📱

--- Scripts/🗺️.js
+++ Scripts/🗺️.js
@@ -32,7 +32,7 @@ static lib = 0;
 // 🗒: It's after T - 'class T {...}  T.init ()'.
 static async Init () {
 	(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",p="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+p);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[p]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-		key: localStorage.getItem(T.name), // Blue
+		key: sessionStorage.getItem(T.name), // Blue
 		v: "weekly",
 		region: "IL",
 		language: "iw",
