1082eb62c484ffe3d7fd0b66858bff554641bd10
Haya-Sixt <azbmdk@gmail.com>
2024-04-30 16:40:18 +0300

    🐈‍⬛📱

--- 📜/🖼️.js
+++ 📜/🖼️.js
@@ -16,17 +16,19 @@ wdgt.Init = ()=> {
 	if ($app.Vars ['🌃'] == 'true') return Carousel ();
 	
 	const Add = async (dir)=> (await (await fetch (`/ls ${wdgt.id}/${dir}`)).json()).forEach ((e)=> gallery.push (e)),
-		a = [], c = $('#🗓️ .tdCurrent:not(.tdCurrentHeb)').text().match(Helpers.Emoji());
+		a = [], c = $('#🗓️ .tdCurrent:not(.tdCurrentHeb)').text().match(Helpers.Emoji()),
+		parasha = $('#🗓️ .tdCurrentHeb .parasha').text ();
 	if (!c) return Carousel (); 
 	
 	let m = $('#🗓️ .tdCurrentHeb').text().match(Helpers.Emoji());
 	m && (m = m.reduce((s, e)=> (s == '' ? '' : `${s},`) + ((c ?? ' ').includes(e) ? '' : e), ''));
 	
-	//m = '🫓,🕯️🕯️'; // TEST
+	if (parasha) parasha = `📖/${parasha}`;
+	if ($app.Vars ['🕯️🕯️'] == "true") m = `${m},${parasha},🕯️🕯️`; 
 	
-	if ($app.Vars ['🕯️🕯️'] == "true") m = `${m ? `${m},` : ``}🕯️🕯️`;
+	//m = '🫓,📖/אחרי מות,🕯️🕯️'; // TEST
 	
-	m && m.split (',').forEach ((e)=> a.push (Add (e)));
+	m && m.split (',').forEach ((e)=> e && a.push (Add (e)));
 	Promise.all (a).then (Carousel);
 }
 
@@ -36,7 +38,7 @@ wdgt.Update = ()=> { };
 //
 function Carousel () { 
 	const c = $(wdgt.sid).parent ().attr ('id'),
-		P = (v)=> $(`#${c}`).css (`--${$app.Constants.Name}-${c}-${wdgt.id}`, v ? `url(${v})` : ''); 
+		P = (v)=> $(`#${c}`).css (`--${$app.Constants.Name}-${c}-${wdgt.id}`, v ? `url("${v}")` : ''); 
 	if (!gallery.length) return P (''); 
 	if (gallery.length > counter) P (gallery [counter++]);
 	if (gallery.length <= counter) counter = 0;
