678c301c3617dc59535dbfd7a03fe9e7f36f8941
Haya-Sixt <azbmdk@gmail.com>
2024-08-29 02:03:35 +0300

    📱

--- 📜/🔮.js
+++ 📜/🔮.js
@@ -6,7 +6,7 @@
 const wdgt = new $app.Widget('🔮', { appendTo: '' });
 
 // Debug
-setTimeout (Ask, 5000)
+//setTimeout (Ask, 5000)
 
 //
 wdgt.Init = manual=> {
@@ -30,87 +30,12 @@ wdgt.Init = manual=> {
 //
 wdgt.Update = ()=> {}
 
+let ask;
 function Ask () {
+	ask?.close ();
 	$app.Widgets['🔔'].Info (`${wdgt.id} Loading...`, "", 20);
-	window['🙊'].OpenInTab ("https://copilot.microsoft.com", { active: false, setParent: true } );
+	ask = window['🙊'].OpenInTab ("https://copilot.microsoft.com", { active: false, setParent: true } );
 }
 
-
-
-/*
-
-//
-wdgt.Add = (id = 'ifr')=> {
-	const sid = `${wdgt.id}_${id}`, url = 'https://copilot.microsoft.com/'// 'https://gemini.google.com';
-	
-	if ($app.Vars ['🕯️🕯️'] == "true") return;
-	
-	//??if ( $(`#${sid} > iframe`).contents ().find ('a').length ) return;
-	
-	top.$ = $;
-	top.$app = $app;
-	top.Helpers = Helpers;
-	
-	IFR (sid, url);
-}
-
-//
-wdgt.Remove = (id = 'ifr')=> {
-	const sid = `${wdgt.id}_${id}`;
-	if (wdgt.data) wdgt.data [id] = wdgt.data [`_${sid}`] = null;
-	$(`#${sid}`).remove ();
-}
-
-
-//
-async function IFR (id, url) {
-	new $app.UIComponent (id, { appendTo: '' });
-	const ifr = $(`#${id}`)
-		.addClass(wdgt.id)
-		.hide ()
-		.append ('<iframe>')
-		.children().first ()
-		.attr ('width', '100%')
-		.attr ('height', '100%')
-		.attr ('frameborder', '0')
-		.attr ('scrolling', 'no')
-		.attr ('allowtransparency', 'true');
-		
-	const b = ifr.contents ()
-		.find ('body')
-		.addClass (wdgt.id)
-		.attr ('onload', `${Load.toString ().replaceAll ('"', "'")} Load('${wdgt.id}', '${id}', '${url}')`); 
-	
-	b.trigger('load'); // 🗒: Iframe.apply(b[0].ownerDocument);
-	//??await Helpers.WaitFor (()=> wdgt?.data?.[`_${id}`]);
-	
-	return b;
-	
-	// 🗒: "Uncaught in promise" - if (!wdgt?.data?.[id]) throw `${wdgt.id}.Map failed`;
-	// 🗒: '"' not allowed! (due to function.toString) 
-	// 🗒: How To Debug ? - use errors (e.g: T.israel = 0), and DevTools...
-	function Load (wdgt_id, id, url) {
-		//document.write ('<body></body>');
-		top ['🙊'].XmlhttpRequest ({
-			method: "GET",
-			url: url,
-			onload: xhr=> {
-				document.open ();
-				document.write (xhr.responseText);
-				document.close ();
-				top.$(`${wdgt_id}`).show ();
-			},
-			onerror: ()=> document.write (`${wdgt_id}.Load.XmlhttpRequest.onerror`)
-		}); 
-		
-		// Auto Remove
-		setTimeout (()=> {
-			top.$app.Widgets [wdgt_id].Remove ();
-		}, 10 * 3000);
-	} // Load
-
-} // IFR
-
-*/
 
 })();
